34a0df9f36f6417bd3fcd8550213a2fe05ad5785
István Szilágyi <istvan.szilagyi@eon.com>
2025-03-06 13:15:26 +0000

    Button development

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast'
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("demo.controller.Home", {
         onInit() {
+        },
+        onPress: () => {
+            MessageToast.show("Nyomd a lóba... 🐴");
         }
     });
 });
