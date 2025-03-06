b25b381e914178e71c1bd8f7bcf4fbff992c29a9
István Szilágyi <istvan.szilagyi@eon.com>
2025-03-06 15:06:03 +0000

    new dialog

--- webapp/controller/MyPanel.controller.js
+++ webapp/controller/MyPanel.controller.js
@@ -1,17 +1,71 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/m/MessageToast'
-], (Controller, MessageToast) => {
+    'sap/m/MessageToast',
+    "sap/m/Dialog",
+    "sap/m/List",
+    "sap/m/StandardListItem",
+    "sap/m/Button",
+    "sap/m/library"
+], (Controller, MessageToast, Dialog, List, StandardListItem, Button, mobileLibrary) => {
     "use strict";
+
+    var ButtonType = mobileLibrary.ButtonType;
 
     return Controller.extend("demo.controller.MyPanel", {
         onInit() {
         },
         onPress() {
-            this.oBundle = this.getView().getModel('i18n').getResourceBundle();
-            const sText = this.oBundle.getText("popUpMessage");
+            const oBundle = this.getView().getModel('i18n').getResourceBundle();
+            const sText = oBundle.getText("popUpMessage");
 
             MessageToast.show(sText);
+        },
+        openDialog() {
+                if (!this.oDefaultDialog) {
+                    this.oDefaultDialog = new Dialog({
+                        title: "Available Products",
+                        content: new List({
+                            items: {
+                                path: "/ProductCollection",
+                                template: new StandardListItem({
+                                    title: "{Name}",
+                                    counter: "{Quantity}"
+                                })
+                            }
+                        }),
+                        beginButton: new Button({
+                            type: ButtonType.Emphasized,
+                            text: "OK",
+                            press: function () {
+                                this.oDefaultDialog.close();
+                            }.bind(this)
+                        }),
+                        endButton: new Button({
+                            text: "Close",
+                            press: function () {
+                                this.oDefaultDialog.close();
+                            }.bind(this)
+                        })
+                    });
+    
+                    // to get access to the controller's model
+                    this.getView().addDependent(this.oDefaultDialog);
+                }
+    
+                this.oDefaultDialog.open();
+        },
+        async openDialog2() {
+            if(!this.dialog) {
+                this.dialog = await this.loadFragment({
+                    name: "demo.view.MyDialog"
+                });
+            }
+            this.dialog.open();
+        },
+        closeDialog() {
+            if(this.dialog) {
+                this.dialog.close();
+            }
         }
     });
 });
