d779cd1f54dcb7f602fd1fe530eddca14e5c4ed2
István Szilágyi <istvan.szilagyi@eon.com>
2025-03-07 13:20:08 +0000

    implemented filter search

--- webapp/controller/TablePanel.controller.js
+++ webapp/controller/TablePanel.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Filter"
+], (Controller, FilterOperator, Filter) => {
     "use strict";
 
     return Controller.extend("demo.controller.RatingPanel", {
@@ -8,6 +10,24 @@ sap.ui.define([
             if(amount>2000) return "Error";
             if(amount<1500) return "Success";
             return "Information"; 
+        },
+        onSearch(event){
+            const value = event.getParameter("newValue");
+            const filters = [];
+            if(value) {
+                filters.push(new Filter("ID", FilterOperator.EQ, value));
+                filters.push(new Filter("SupplierName", FilterOperator.Contains, value.toString()));
+            }
+            const table = this.getView().byId('invoicesTable');
+            const binding = table.getBinding("items");
+            if(filters.length > 0){
+                binding.filter(new Filter({
+                    filters: filters,
+                    and: false
+                }));
+            } else {
+                binding.filter([]);
+            }
         }
     });
 });
